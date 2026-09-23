8eb71b854025abc43476ac84eb603684a7713fce
vaani <vaaniv41@gmail.com>
2026-09-23 12:51:09 +0530

    yes

--- backend/src/app.js
+++ backend/src/app.js
@@ -7,8 +7,7 @@ const app = express()
 const port = process.env.PORT || 4000
 
 app.use(express.json())
-app.use(cors())
-app.use(router)
+
 
 app.get("/health", (req, res) => {
   res.status(200).json({
@@ -16,6 +15,9 @@ app.get("/health", (req, res) => {
     message: "Kanban backend is healthy"
   });
 });
+
+app.use(cors())
+app.use(router)
 
 app.listen(port, ()=>{
     console.log(`Server is running on port ${port}`);
